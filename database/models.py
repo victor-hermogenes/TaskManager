45205dfef87580e158259c2629fcf56fbe0d6cd8
victor-hermogenes <victorltrgh@gmail.com>
2024-06-25 14:23:50 -0300

    Correcting typos

--- database/models.py
+++ database/models.py
@@ -10,7 +10,7 @@ def create_tables(conn):
     cursor = conn.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
-        id INTENGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
     )''')
