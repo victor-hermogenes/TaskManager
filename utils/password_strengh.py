79105d6ac834ac33d402a4de8bda38c6c27631ca
victor-hermogenes <victorltrgh@gmail.com>
2024-06-27 12:49:27 -0300

    Creating new py file to create evaluate_password_strength to see if password is strong enough.

---
+++ utils/password_strengh.py
@@ -0,0 +1,16 @@
+import re
+
+
+def evaluate_password_strength(password):
+    strength = 0
+    if len(password) >= 8:
+        strength += 1
+    if re.search(r"[A-Z]", password):
+        strength += 1
+    if re.search(r"[a-z]", password):
+        strength += 1
+    if re.search(r"\d", password):
+        strength += 1
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        strength += 1
+    return strength
