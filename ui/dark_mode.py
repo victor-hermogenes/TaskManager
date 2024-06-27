0aaee4be8b02b0cc79835459f2f2bcce62de0940
victor-hermogenes <victorltrgh@gmail.com>
2024-06-27 15:29:56 -0300

    Enhancing dark mode

--- ui/dark_mode.py
+++ ui/dark_mode.py
@@ -1,4 +1,40 @@
-import qdarkstyle
-
 def apply_dark_mode(app):
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    dark_stylesheet = """
+    QWidget {
+        background-color: #2d2d2d;
+        color: #d3d3d3;
+        font-size: 14px;
+    }
+    QMenuBar {
+        background-color: #2d2d2d;
+    }
+    QMenuBar::item {
+        background-color: #2d2d2d;
+        color: #d3d3d3;
+    }
+    QMenuBar::item:selected{
+        background-color: #333333
+    }
+    QPushButton {
+        background-color: #3d3d3d;
+        border: 1px solid #5d5d5d;
+        padding: 5px;
+        border-radius: 5px;
+        color: #FFFFFF
+    }
+    QPushButton:hover {
+        background-color: #5d5d5d;
+    }
+    QLineEdit, QComboBox, QDateEdit, QTextEdit {
+        background-color: #3d3d3d;
+        border: 1px solid #5d5d5d;
+        color: #d3d3d3;
+        padding: 5px;
+        border-radius: 5px;
+    }
+    QLineEdite:disabled, QComboBox:disabled, QDateEdit:disabled, QTextEdit:disabled {
+        color: #555555;
+    }
+    """
+
+    app.setStyleSheet(dark_stylesheet)
