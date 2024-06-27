fc909c92015301852083d82c7ca8e365ce576270
victor-hermogenes <victorltrgh@gmail.com>
2024-06-27 11:23:06 -0300

    Fixing auth register bug

--- auth/auth.py
+++ auth/auth.py
@@ -18,17 +18,11 @@ def register_user(username, password):
         cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
         conn.commit()
     except sqlite3.IntegrityError:
-        return False    # Username already exists
-    
-    conn = get_db_connection()
-    cursor = conn.cursor()
+        conn.close()
+        return False  # Username already exists
 
-    cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
-    result = cursor.fetchone()
     conn.close()
-
-    if result is None:
-        return False    # User not found
+    return True
 
 
 def login_user(username, password):
